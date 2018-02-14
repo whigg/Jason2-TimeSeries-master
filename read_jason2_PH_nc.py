5a85679d41e09215e67c2632dc7c23a7ee38a57e
wosleepy <wosleepy@gmail.com>
2018-02-14 11:56:06 +0100

    minor fit

--- read_jason2_PH_nc.py
+++ read_jason2_PH_nc.py
@@ -11,7 +11,7 @@ from netCDF4 import Dataset
 
 # Read NetCDF3 data, 'r' -- readable only
 """
- # careful with fname. TO DO #
+ careful with fname. TO DO #
 """
 #fname = Dataset('./nc_data/225/cycle000/JA2_GPS_2PdP000_225_20080710_211338_20080710_220951.nc', 'r')
 
@@ -29,29 +29,29 @@ def read_jason2_PH_nc(fname): # name after Tourian's matlab code
         Variables.append(i) # Save all Variables
         try:
             attval = fname.variables[i].units # When Variables have units
-        except AttributeError: 
+        except AttributeError:
             attval = None # When Variables don't have units, set Attval as None
         Attval.append(attval) # Save Attribute Units(Attval)
-    
+
         for j in [attval]: # Looking for values in Attribute Units(Attval)
             if j == None:
                 attname = None # Attribute have no units, set Attname as None
             else:
                 attname = 'units' # Attribute have units, set Attname as 'units'
             Attname.append(attname) # Save Attribute Name(Attname)
-    
-        get_data_matrix = fname.variables[i][:] 
+
+        get_data_matrix = fname.variables[i][:]
         DataMatrix.append(get_data_matrix) # Save Data Matrix(DataMatrix)
         MatrixShape.append(get_data_matrix.shape) # Save Matrix Shape(MatrixShape)
 
-    ncData = pd.DataFrame({'Variables': Variables, 
+    ncData = pd.DataFrame({'Variables': Variables,
                            'AttributeName': Attname,
-                           'AttributeValue': Attval, 
-                           'DataMatrix': DataMatrix,  
-                           'MatrixShape': MatrixShape})   
-    
+                           'AttributeValue': Attval,
+                           'DataMatrix': DataMatrix,
+                           'MatrixShape': MatrixShape})
+
 #  Possible bug: e.g. shape of 'amplitude_20hz_ku_mle3' in Matlab 20x2963, Python 2963x20
-#  Solved by check m = shape_matrix[0] & n != 20                               
+#  Solved by check m = shape_matrix[0] & n != 20
 
     data_row_num = ncData.at[0, 'MatrixShape'][0] # Number of row in final data matrix
     data = np.empty([data_row_num,138]) # create empty space for final data matrix
@@ -66,45 +66,28 @@ def read_jason2_PH_nc(fname): # name after Tourian's matlab code
         except ValueError: # 1D matrix set n as 1
             m = shape_matrix[0]
             n = 1
-    
+
         if i == 1: # set c = length of time matrix
             c = m
-    
+
         if m == c and n != 20: # choose the data, we don't use 20hz data here
             header_usage.append(1) # mark used as 1
-            data[:, s] = ncData.at[i-1, 'DataMatrix'] 
+            data[:, s] = ncData.at[i-1, 'DataMatrix']
             s = s + 1
         else:
-            header_usage.append('Unused') # mark unused as 'Unused'
-         
+            header_usage.append(None) # mark unused as None
+
     header_usage = pd.Series(header_usage) # change header_usage to pandas.Series
     header = pd.concat([header, header_usage], axis=1) # concatenate with header
 
     return header, data
 
 
-def Altprocess():
-    x = Dataset('./nc_data/225/cycle000/JA2_GPS_2PdP000_225_20080710_211338_20080710_220951.nc', 'r')
-    y1, y2 = read_jason2_PH_nc(x)
-    print(y1.shape, y2.shape)
+#def Altprocess():
+#    x = Dataset('./nc_data/225/cycle000/JA2_GPS_2PdP000_225_20080710_211338_20080710_220951.nc', 'r')
+#    y1, y2 = read_jason2_PH_nc(x)
+#    print(y1.shape, y2.shape)
 
 
-a = 1
-Altprocess()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#a = 1
+#Altprocess()
